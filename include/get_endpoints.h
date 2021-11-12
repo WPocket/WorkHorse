b1e7c0a89fa0dbb583f56280184d5ae238f32f80
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2021-11-12 13:46:08 -0500

    removed unnecessary code

--- include/get_endpoints.h
+++ include/get_endpoints.h
@@ -13,18 +13,6 @@ typedef struct {
 	int ram;
 } Endpoint;
 
-typedef struct {
-	Endpoint *endpoints;
-	int count;
-} Endpoints;
 
-// TODO use memcopy for this
-void addEndpoint(Endpoint* endpoint);
-
-void delEndpoint(int index);
-
-Endpoint* getBestFree(Endpoints endpoints);
-
-Endpoints* getEndpoints(char* db_address, struct _u_instance *instance, int db_index);
 
 #endif
