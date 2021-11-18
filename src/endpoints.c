3eb4ffa21f60b82832dfe621dcaf8040a2a6cc16
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2021-11-18 16:49:50 -0500

    properly parsing entire endpoint

--- src/endpoints.c
+++ src/endpoints.c
@@ -8,6 +8,8 @@
 
 #include <jansson.h>
 
+#define is_string
+
 #define STRINGS_LEN 5
 Endpoint *json_to_endpoint(json_t *json) {
   if (json->type != 0) {
@@ -15,26 +17,73 @@ Endpoint *json_to_endpoint(json_t *json) {
   }
   // map it into an endpoint
   Endpoint *endpt = malloc(sizeof(Endpoint));
-  json_t *value = json_object_get(json, "ip");
-  if (value == NULL) {
-    printf("value is null\n");
-    return NULL;
+  json_t *value;
 
-  } else if (value->type != JSON_STRING) {
-
-    printf("value is not a string\n");
-    return NULL;
-  } else {
-    endpt->ip = strdup(json_string_value(value));
-    if (endpt->ip == NULL) {
-      goto free_endpt;
-    }
-    return endpt;
-  free_endpt:
-    free(endpt);
-    return NULL;
+  // attempt to build an endpoint definition
+  // get ip
+  value = json_object_get(json, "ip");
+  if (value == NULL || value->type != JSON_STRING) {
+    goto free_endpt;
   }
+  const char *temporary = json_string_value(value);
+  if (temporary == NULL) {
+    goto free_endpt;
+  }
+
+  endpt->ip = strdup(temporary);
+
+  // get busy
+  value = json_object_get(json, "busy");
+  if (value == NULL ||
+      (value->type != JSON_TRUE && value->type != JSON_FALSE)) {
+    free(endpt->ip);
+    goto free_endpt;
+  }
+
+  endpt->busy = (value->type == JSON_TRUE);
+
+  // get cores
+  value = json_object_get(json, "cores");
+  if (value == NULL || value->type != JSON_INTEGER) {
+    free(endpt->ip);
+    goto free_endpt;
+  }
+
+  endpt->cores = json_integer_value(value);
+  // get cores
+  value = json_object_get(json, "speed");
+  if (value == NULL || value->type != JSON_INTEGER) {
+    free(endpt->ip);
+    goto free_endpt;
+  }
+
+  endpt->speed = json_integer_value(value);
+
+  // get cores
+  value = json_object_get(json, "ram");
+  if (value == NULL || value->type != JSON_INTEGER) {
+    free(endpt->ip);
+    goto free_endpt;
+  }
+
+  endpt->ram = json_integer_value(value);
+
+  return endpt;
+free_endpt:
+  free(endpt);
+  return NULL;
 }
+
+void print_endpoint(Endpoint *endpoint) {
+  printf("{\n");
+  printf("\"ip\": \"%s\",\n", endpoint->ip);
+  printf("\"busy\": %s,\n", (endpoint->busy) ? "true" : "false");
+  printf("\"cores\": %d,\n", endpoint->cores);
+  printf("\"speed\": %d,\n", endpoint->speed);
+  printf("\"ram\": %d,\n", endpoint->ram);
+  printf("}\n");
+}
+
 // TODO this takes values in x-www-form-urlencoded make this use json
 /**
  * @brief the endpoint for a post request to "/post"
@@ -55,8 +104,8 @@ int post_endpoint(const struct _u_request *request,
     if (endpt == NULL) {
       goto endpt_null;
     }
-    printf("the value of ip is: %s\n", endpt->ip);
-    response_body = strdup(endpt->ip);
+    // print the endpoint
+    print_endpoint(endpt);
 
   } else if (error != NULL) {
     printf("error occurred: %s", error->text);
